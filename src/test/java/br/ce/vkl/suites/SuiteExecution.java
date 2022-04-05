893588db86e0ef8c453ec1b5e576e90d9e3ab6b9
viktor-lima <joaolimak10@outlook.com>
2022-04-05 13:54:59 -0300

    ->

--- src/test/java/br/ce/vkl/suites/SuiteExecution.java
+++ src/test/java/br/ce/vkl/suites/SuiteExecution.java
@@ -1,14 +1,12 @@
 package br.ce.vkl.suites;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import br.ce.vkl.servicos.CalculationRentValue;
 import br.ce.vkl.servicos.CalculatorTest;
 import br.ce.vkl.servicos.LocacaoServiceTest;
 
-@RunWith(Suite.class)
+//@RunWith(Suite.class)
 @SuiteClasses({
 	CalculatorTest.class,
 	CalculationRentValue.class,
